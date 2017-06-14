2412fe724471a9e787ff4caa338b754b822f4e88
trujunzhang <trujunzhang@gmail.com>
2017-06-14 19:07:21 +0800

    Redux with parse.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,17 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
-import auth from './auth';
 import error from './error';
 
 import Telescope from '../lib/en_US';
 const messages = Telescope.strings['en'] || {};
 
 const rootReducer = combineReducers({
-    auth,
     error,
     posts: require('./posts'),
     topics: require('./topics'),
-    routing: routerReducer
+    routing: routerReducer,
+    auth: require('./auth/authReducer')
 });
 
 export default rootReducer;
