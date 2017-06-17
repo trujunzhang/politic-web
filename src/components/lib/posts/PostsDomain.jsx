3282622fa062a25c1d1e38a935077dc0f21de7f8
trujunzhang <trujunzhang@gmail.com>
2017-06-17 22:29:06 +0800

    Parse with react.js.

--- src/components/lib/posts/PostsDomain.jsx
+++ src/components/lib/posts/PostsDomain.jsx
@@ -1,27 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react'
 import { Link } from 'react-router'
 
+/**
+ * A: Remove “THE-VIEWSPAPER” from there
+ * B: YES
+ * A: “theviewspaper.net” on a line between the title and read more and link it to the domain page
+ * B: “theviewspaper.net” on a line between the title and read more and link it to the domain page, need to add click event?
+ * A: YES
+ */
+const PostsDomain = ({post, domainClass}) => {
+  let domain = (post.sourceFrom || '').replace('www.', '')
 
-class PostsDomain extends Component {
-    // A: Remove “THE-VIEWSPAPER” from there
-    // B: YES
-    // A: “theviewspaper.net” on a line between the title and read more and link it to the domain page
-    // B: “theviewspaper.net” on a line between the title and read more and link it to the domain page, need to add click event?
-    // A: YES
-    render() {
-        const {post, domainClass} = this.props;
-        return (
-            <span className={`domain_item ${domainClass}`}>
-                <Link to="/from/">
-                    <span className="domain">
-                        {(post.sourceFrom || '').replace('www.', '')}
-                    </span>
-                </Link>
-            </span>
-        )
-    }
+  return (
+    <span className={`domain_item ${domainClass}`}>
+      <Link to={`/from/${domain}`}>
+        <span className="domain">{domain}</span>
+      </Link>
+    </span>
+  )
 }
 
 export default PostsDomain
-
 
