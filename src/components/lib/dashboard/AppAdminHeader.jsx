060bace847b4a231fee41236c45cffad6360d3fa
trujunzhang <trujunzhang@gmail.com>
2017-06-18 09:26:28 +0800

    Parse with react.js.

--- src/components/lib/dashboard/AppAdminHeader.jsx
+++ src/components/lib/dashboard/AppAdminHeader.jsx
@@ -1,7 +1,6 @@
 import Telescope from '../index'
 import React, { Component } from 'react'
 import Users from '../../../lib/users'
-import moment from 'moment'
 import { Link } from 'react-router'
 import Avatar from 'react-avatar'
 
@@ -14,80 +13,11 @@ class AppAdminHeader extends Component {
     }
   }
 
-  componentDidMount () {
-    // $('#right-user-menu').hover(
-    //   function () {
-    //     $(this).addClass('open')
-    //   },
-    //   function () {
-    //     $(this).removeClass('open')
-    //   }
-    // )
-  }
-
-  onToggleSidebarIconClick () {
-    // if (this.state.isOpenSidebar) {
-    //   $('#admin-dashboard').removeClass('hold-transition skin-blue sidebar-mini').addClass('skin-blue sidebar-mini sidebar-collapse')
-    // } else {
-    //   $('#admin-dashboard').removeClass('skin-blue sidebar-mini sidebar-collapse').addClass('hold-transition skin-blue sidebar-mini')
-    // }
-    // this.setState({isOpenSidebar: !this.state.isOpenSidebar})
-  }
-
-  renderLoggedUser () {
-    const {currentUser} = this.props
-    const name = Users.getDisplayName(currentUser)
-
-    return (
-      <li className="dropdown user user-menu" id="right-user-menu">
-        <a className="dropdown-toggle">
-          <div className="row">
-            <div className="col-lg-4">
-              <Telescope.components.UsersMenu currentUser={currentUser}/>
-            </div>
-            <div className="col-lg-8">
-              <span className="hidden-xs">{name}</span>
-            </div>
-          </div>
-        </a>
-        <ul className="dropdown-menu">
-          <li className="user-header">
-            <img src="" className="img-circle" alt="User Image"/>
-            <p>
-              {name}
-              <small>Member since Nov. 2012</small>
-            </p>
-          </li>
-          <li className="user-footer">
-            <div className="pull-left">
-              <a className="btn btn-default btn-flat">Profile</a>
-            </div>
-            <div className="pull-right">
-              <a className="btn btn-default btn-flat">Sign out</a>
-            </div>
-          </li>
-        </ul>
-      </li>
-    )
-  }
-
-  renderToggleSidebarIcon () {
-    return (
-      //admin-dashboard
-      <a className="sidebar-toggle" id="sidebar-toggle" role="button"
-         onClick={this.onToggleSidebarIconClick.bind(this)}>
-        <span className="sr-only">Toggle navigation</span>
-      </a>
-    )
-  }
-
-  renderAdminbarLeft () {
+  renderLeft () {
     return (
       <ul id="wp-admin-bar-root-default" className="ab-top-menu">
         <li id="wp-admin-bar-site-name" className="menupop">
-          <Link className="ab-item" to="/">
-            {'Politicl'}
-          </Link>
+          <Link className="ab-item" to="/">{'Politicl'}</Link>
         </li>
         <li id="wp-admin-bar-comments">
           <a className="ab-item"
@@ -95,12 +25,8 @@ class AppAdminHeader extends Component {
                this.context.messages.appManagement.pushAdminSidebar(this.props.router, 'comments')
              }}
           >
-            <span className="ab-icon">
-            </span>
-            <span id="ab-awaiting-mod"
-                  className="ab-label awaiting-mod pending-count count-1">
-                          1
-            </span>
+            <span className="ab-icon"/>
+            <span id="ab-awaiting-mod" className="ab-label awaiting-mod pending-count count-1">1</span>
             <span className="screen-reader-text">
               1 comment awaiting moderation
             </span>
@@ -110,7 +36,7 @@ class AppAdminHeader extends Component {
     )
   }
 
-  renderAdminbarRight () {
+  renderRight () {
     const {currentUser} = this.props,
       avatarObj = Users.getAvatarObj(currentUser),
       {my_account_hover} = this.state,
@@ -160,8 +86,8 @@ class AppAdminHeader extends Component {
     return (
       <div id="wpadminbar">
         <div className="quicklinks" id="wp-toolbar" role="navigation">
-          {this.renderAdminbarLeft()}
-          {this.renderAdminbarRight()}
+          {this.renderLeft()}
+          {this.renderRight()}
         </div>
         <a className="screen-reader-shortcut"
            href="http://localhost:8444/wp-login.php?action=logout&amp;_wpnonce=f334df5e37">Log Out</a>
