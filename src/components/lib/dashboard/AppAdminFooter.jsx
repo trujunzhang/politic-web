04fc8cc6156dfb63d2f3f5e5ceeb0259309df4dc
trujunzhang <trujunzhang@gmail.com>
2017-06-19 06:34:39 +0800

    Parse with react.js.

--- src/components/lib/dashboard/AppAdminFooter.jsx
+++ src/components/lib/dashboard/AppAdminFooter.jsx
@@ -5,13 +5,12 @@ import moment from 'moment'
 class AppAdminFooter extends Component {
 
   render () {
-    const year = moment(new Date()).format('YYYY')
     return (
       <footer className="main-footer">
         <div className="pull-right hidden-xs">
           <b>Version</b> 3.0.1
         </div>
-        <strong>{'Copyright © ' + year + ' '}</strong> All rights reserved.
+        <strong>{`Copyright © ${moment(new Date()).format('YYYY')}  `}</strong> All rights reserved.
       </footer>
     )
   }
