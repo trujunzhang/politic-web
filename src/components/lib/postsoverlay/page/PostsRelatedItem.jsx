72416189018a1880639c18fa25ac38ee5c4dd07d
trujunzhang <trujunzhang@gmail.com>
2017-06-19 21:21:13 +0800

    Parse with react.js.

--- src/components/lib/postsoverlay/page/PostsRelatedItem.jsx
+++ src/components/lib/postsoverlay/page/PostsRelatedItem.jsx
@@ -2,77 +2,87 @@ import Telescope from '../../../lib'
 import React, { Component } from 'react'
 import Posts from '../../../../lib/posts'
 
+const {pushOverlayDetailedPost} = require('../../../../actions').default
+
 class PostsRelatedItem extends Component {
 
-    renderVotesSection () {
-        const {post} = this.props
-        return (
-            <div className="smallSize_center secondaryText_PM80d">
-                <div className="row">
-                    <Telescope.components.RelatedPostUpvote post={post}/>
-                </div>
-                <div className="row">
-                    <Telescope.components.RelatedPostDownvote post={post}/>
-                </div>
-            </div>
-        )
-    }
+  renderVotesSection () {
+    const {post} = this.props
+    return (
+      <div className="smallSize_center secondaryText_PM80d">
+        <div className="row">
+          <Telescope.components.RelatedPostUpvote post={post}/>
+        </div>
+        <div className="row">
+          <Telescope.components.RelatedPostDownvote post={post}/>
+        </div>
+      </div>
+    )
+  }
 
-    renderThumbnail () {
-        const {post} = this.props,
-              imageSet = Posts.getThumbnailSet(post)
+  renderThumbnail () {
+    const {post} = this.props,
+      imageSet = Posts.getThumbnailSet(post)
 
-        if (imageSet.small) {
-            return (
-                <div className="thumbnail_2Blny">
-                    <div className="post-thumbnail related-thumbnail" onClick={this.onRelatedPostClick.bind(this)}>
-                        <a className="container_22rD3 related-thumbnail">
-                            <Telescope.components.BlurryImage
+    if (imageSet.small) {
+      return (
+        <div className="thumbnail_2Blny">
+          <div className="post-thumbnail related-thumbnail" onClick={this.onRelatedPostClick.bind(this)}>
+            <a className="container_22rD3 related-thumbnail">
+              <Telescope.components.BlurryImage
                 imageId={post._id + '-thumbnail'}
                 containerClass={'container__Ql6q lazyLoadContainer_3KgZD'}
                 imageClass={'post-list-thumbnail'}
                 imageSet={imageSet}
                 imageTitle={post.title}
                 width={62}
-                height={62}
-                            />
-                        </a>
-                    </div>
-                </div>
-            )
-        }
-
-        return null
+                height={62}/>
+            </a>
+          </div>
+        </div>
+      )
     }
 
-    render () {
-        const {post} = this.props
+    return null
+  }
 
-        return (
-            <div className="link_2Cj8i">
-                <div className="side_3fRtk related-left-panel">
-                    {this.renderThumbnail()}
-                    {this.renderVotesSection()}
-                </div>
-                <a onClick={this.onRelatedPostClick.bind(this)}>
-                    <h2 className="name_DrXo8 featured_2W7jd default_tBeAo base_3CbW2">{post.title}</h2>
-                    <p className="text_relate_3Wjo0 subtle_1BWOT base_3CbW2">
-                        {Posts.getLimitedContent(post.body, 100)}
-                    </p>
-                </a>
-            </div>
-        )
-    }
+  render () {
+    const {post} = this.props
 
-    onRelatedPostClick (event) {
-        event.preventDefault()
+    return (
+      <div className="link_2Cj8i">
+        <div className="side_3fRtk related-left-panel">
+          {this.renderThumbnail()}
+          {this.renderVotesSection()}
+        </div>
+        <a onClick={this.onRelatedPostClick.bind(this)}>
+          <h2 className="name_DrXo8 featured_2W7jd default_tBeAo base_3CbW2">{post.title}</h2>
+          <p className="text_relate_3Wjo0 subtle_1BWOT base_3CbW2">
+            {Posts.getLimitedContent(post.body, 100)}
+          </p>
+        </a>
+      </div>
+    )
+  }
 
-        // const {currentUser, messages} = this.context,
-        //   {router, post} = this.props,
-        //   admin = messages.appManagement.getAdmin(this.props.location, currentUser)
-        // messages.pushRouterForDetailPage(router, post, admin)
-    }
+  onRelatedPostClick (event) {
+    event.preventDefault()
+
+    this.props.dispatch(pushOverlayDetailedPost(this.props.post))
+
+    // const {currentUser, messages} = this.context,
+    //   {router, post} = this.props,
+    //   admin = messages.appManagement.getAdmin(this.props.location, currentUser)
+    // messages.pushRouterForDetailPage(router, post, admin)
+  }
 
 }
 
-export default PostsRelatedItem
+/**
+ * ## Imports
+ *
+ * Redux
+ */
+import { connect } from 'react-redux'
+
+export default connect()(PostsRelatedItem)
