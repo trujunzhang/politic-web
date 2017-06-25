0bba87268a0aa2780b5eb833ed21d59b7dc81f8e
trujunzhang <trujunzhang@gmail.com>
2017-06-25 22:03:51 +0800

    Parse.

--- src/components/lib/actions/TopicItem.jsx
+++ src/components/lib/actions/TopicItem.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router'
+import {Link} from 'react-router'
+
+const {dismissPopModel} = require('../../../actions').default
 
 const TopicItem = ({topic}) => {
   return (
     <Link title={topic.name}
+          onClick={(e) => {
+            this.props.dispatch(dismissPopModel())
+          }}
           className="button_2I1re smallSize_1da-r secondaryText_PM80d greySolidColor_270pZ solidVariant_2wWrf"
           to={`/topic/${topic.id}/${topic.name}`}>
       <div className="buttonContainer_wTYxi">{topic.name}</div>
@@ -11,4 +16,17 @@ const TopicItem = ({topic}) => {
   )
 }
 
-export default TopicItem
+
+/**
+ * ## Imports
+ *
+ * Redux
+ */
+var {connect} = require('react-redux')
+
+/**
+ * Connect the properties
+ */
+
+export default connect()(TopicItem)
+
