f850f38ed84603d215ade709e1265bad0cf7566c
trujunzhang <trujunzhang@gmail.com>
2017-06-28 09:28:54 +0800

    Parse.

--- cloud/main.js
+++ cloud/main.js
@@ -22,7 +22,69 @@
 
 'use strict'
 
-Parse.Cloud.afterSave('User', function (request, response) {
+/**
+ * "mailUrl": "smtp://politiclmailsender:Politicl@123@smtp.sendgrid.net:465/",
+ */
+Parse.Cloud.define("sendEmail", function (request, response) {
 
+  // Import SendGrid module and call with your SendGrid API Key
+  var sg = require('sendgrid')('your SendGrid API key here');
 
-})
+  // Create the SendGrid Request
+  var reqSG = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: {
+      personalizations: [
+        {
+          to: [
+            {
+              // This field is the "to" in the email
+              email: request.params.toEmail,
+            },
+          ],
+          // This field is the "subject" in the email
+          subject: request.params.subject,
+        },
+      ],
+      // This field contains the "from" information
+      from: {
+        email: 'info@youremail.com',
+        name: 'Your Name',
+      },
+      // This contains info about the "reply-to"
+      // Note that the "reply-to" may be different than the "from"
+      reply_to: {
+        email: 'info@youremail.com',
+        name: 'Your Name',
+      },
+      content: [
+        {
+          // You may want to leave this in text/plain,
+          // Although some email providers may accept text/html
+          type: 'text/plain',
+          // This field is the body of the email
+          value: request.params.body,
+        },
+      ],
+    },
+  });
+
+  // Make a SendGrid API Call
+  sg.API(reqSG, function (SGerror, SGresponse) {
+    // Testing if some error occurred
+    if (SGerror) {
+      // Ops, something went wrong
+      console.error('Error response received');
+      console.error('StatusCode=' + SGresponse.statusCode);
+      console.error(JSON.stringify(SGresponse.body));
+      response.error('Error = ' + JSON.stringify(SGresponse.body));
+    }
+    else {
+      // Everything went fine
+      console.log('Email sent!');
+      response.success('Email sent!');
+    }
+  });
+
+});
