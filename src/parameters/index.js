517ea0d3465c46174f37edac65c50d2d17ad824b
trujunzhang <trujunzhang@gmail.com>
2017-06-29 11:53:06 +0800

    Parse.

--- src/parameters/index.js
+++ src/parameters/index.js
@@ -2,6 +2,7 @@ var Parameters = {}
 
 Parameters.Posts = require('./posts').default
 Parameters.Users = require('./users').default
+Parameters.Topics = require('./topics').default
 
 export default Parameters
 
